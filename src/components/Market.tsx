142954565e9a6151f44ad3a8076171f2c6bdba57
deusexmachinaa <gjfzml@gmail.com>
2023-05-23 05:38:41 +0900

    이미지 추가

--- src/components/Market.tsx
+++ src/components/Market.tsx
@@ -1,14 +1,17 @@
 // components/Market.tsx
+import Image from "next/image";
 import React from "react";
+import upbit from "/public/upbit.webp";
+import binance from "/public/binance.webp";
 
 export default function Market() {
   return (
     <div className="sm:flex sm:items-center sm:justify-between">
       <div className="relative flex items-center justify-between gap-10 mb-4 text-center sm:mb-0">
-        <div className="relative w-full text-sm rounded-md dark:bg-neutral-800">
-          <button className="w-full px-1 py-3 border border-gray-200 rounded-md dark:border-neutral-700 [&>*]:justify-center">
+        <div className="relative w-full bg-white border border-gray-200 rounded-md dark:border-neutral-700 dark:bg-neutral-800 sm:w-40">
+          <button className="w-full px-4 py-3 border border-gray-200 rounded-md dark:border-neutral-700 [&>*]:justify-center">
             <div className="flex items-center h-5 gap-2">
-              {/* <img src="/images/krw.png" alt="KRW" className="w-5 h-5" /> */}
+              <Image alt="upbit krw" src={upbit} width={20} height={20} />
               <span className="text-xs">업비트 KRW</span>
             </div>
           </button>
@@ -35,18 +38,23 @@ export default function Market() {
           </ul>
         </div>
         <i className="absolute text-xs -translate-x-1/2 fa-solid fa-right-left left-1/2"></i>
-        <div className="relative w-full text-sm rounded-md dark:bg-neutral-800">
+        <div className="relative w-full bg-white border border-gray-200 rounded-md dark:border-neutral-700 dark:bg-neutral-800 sm:w-40">
           <button className="w-full px-4 py-3 border border-gray-200 rounded-md dark:border-neutral-700 [&>*]:justify-center">
-            <div className="flex items-center h-5 gap-2">
-              {/* <img alt="binance usdt" fetchpriority="high" width="20" height="20" decoding="async" data-nimg="1" style="color:transparent" srcset="/_next/image?url=%2Fimages%2Fbinance.webp&amp;w=32&amp;q=75 1x, /_next/image?url=%2Fimages%2Fbinance.webp&amp;w=48&amp;q=75 2x" src="/_next/image?url=%2Fimages%2Fbinance.webp&amp;w=48&amp;q=75"> */}
+            <div className="flex flex-row items-center h-5 gap-2">
+              <Image alt="binance usdt" src={binance} width={20} height={20} />
               <span className="text-xs">바이낸스 USDT</span>
             </div>
           </button>
           <ul className="dark:bg-neutral-800 dark:border-neutral-700 flex flex-col gap-1 border border-gray-200 rounded-md mt-1 p-2 absolute bg-white w-full z-10 hidden">
-            <li className="px-4 py-3 rounded-md cursor-pointer hover:bg-gray-200 dark:hover:bg-neutral-700 relative flex items-center justify-center bg-gray-200 dark:bg-neutral-700">
+            <li className="px-5 py-3 rounded-md cursor-pointer hover:bg-gray-200 dark:hover:bg-neutral-700 relative flex items-center justify-center bg-gray-200 dark:bg-neutral-700">
               <i className="absolute text-xs left-2 fa-solid fa-check"></i>
-              <div className="flex items-center h-5 gap-2">
-                {/* <img alt="binance usdt" fetchpriority="high" width="20" height="20" decoding="async" data-nimg="1" style="color:transparent" srcset="/_next/image?url=%2Fimages%2Fbinance.webp&amp;w=32&amp;q=75 1x, /_next/image?url=%2Fimages%2Fbinance.webp&amp;w=48&amp;q=75 2x" src="/_next/image?url=%2Fimages%2Fbinance.webp&amp;w=48&amp;q=75"> */}
+              <div className="flex flex-row items-center h-5 gap-2">
+                <Image
+                  alt="binance usdt"
+                  src={binance}
+                  width={20}
+                  height={20}
+                />
                 <span className="text-xs">바이낸스 USDT</span>
               </div>
             </li>
@@ -64,7 +72,7 @@ export default function Market() {
           <i className="absolute -translate-y-1/2 top-1/2 fa-solid fa-magnifying-glass left-2"></i>
           <input
             placeholder="비트코인, btc"
-            className="w-40 px-8 py-2 border border-gray-200 rounded-md dark:bg-neutral-800 focus:outline-none dark:border-neutral-700 sm:w-60"
+            className="w-100 px-8 py-2 border border-gray-200 rounded-md dark:bg-neutral-800 focus:outline-none dark:border-neutral-700 sm:w-70"
             defaultValue=""
           />
         </div>
