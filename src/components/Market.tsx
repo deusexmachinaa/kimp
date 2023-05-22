7535bfbba696bd79dd22bd9e1383da56b75c3b74
deusexmachinaa <gjfzml@gmail.com>
2023-05-23 03:59:34 +0900

    value to defaultvalue

--- src/components/Market.tsx
+++ src/components/Market.tsx
@@ -65,7 +65,7 @@ export default function Market() {
           <input
             placeholder="비트코인, btc"
             className="w-40 px-8 py-2 border border-gray-200 rounded-md dark:bg-neutral-800 focus:outline-none dark:border-neutral-700 sm:w-60"
-            value=""
+            defaultValue=""
           />
         </div>
       </div>
