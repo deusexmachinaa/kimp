5e729306d25589e2ad75334662c87ba04c4391b3
deusexmachinaa <gjfzml@gmail.com>
2023-05-23 04:00:25 +0900

    전역상태관리 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,20 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { create } from "zustand";
+
+export type exchangeState = {
+  exchangeRate: number;
+  setExchangeRate: (rate: number) => void;
+};
+// Define your store
+export const useStore = create<exchangeState>((set) => ({
+  exchangeRate: 0,
+  setExchangeRate: (rate: number) => set({ exchangeRate: rate }),
+}));
 
 export default function Header() {
-  const [exchangeRate, setExchangeRate] = useState<number>(0);
+  const exchangeRate = useStore((state) => state.exchangeRate);
+  const setExchangeRate = useStore((state) => state.setExchangeRate);
 
   useEffect(() => {
     fetch(
@@ -13,7 +25,6 @@ export default function Header() {
         setExchangeRate(data[0].basePrice);
       });
   }, []);
-
   return (
     <header className="p-2 border-b dark:border-neutral-700">
       <div className="w-full max-w-screen-lg m-auto text-xs">
