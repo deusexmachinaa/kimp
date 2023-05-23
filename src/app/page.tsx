f29d2fff3b4d1afaa52ec07b794d53ff28678b95
deusexmachinaa <gjfzml@gmail.com>
2023-05-23 21:13:40 +0900

    footer 경로 오류수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 "use client";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Market from "@/components/Market";
 import Nav from "@/components/Nav";
 import Table from "@/components/Table";
-import Footer from "@/components/footer";
 import Script from "next/script";
 import React from "react";
 
