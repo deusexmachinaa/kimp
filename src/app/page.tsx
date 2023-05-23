14d8da14d5cef842767be593970e3556aeb69a5a
deusexmachinaa <gjfzml@gmail.com>
2023-05-23 19:47:57 +0900

    애드센스

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,17 @@ import Header from "@/components/Header";
 import Market from "@/components/Market";
 import Nav from "@/components/Nav";
 import Table from "@/components/Table";
+import Script from "next/script";
 import React from "react";
 
 export default function HomePage() {
   return (
     <>
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8390086553135895"
+        crossOrigin="anonymous"
+      />
       <Header />
       <Nav />
       <article className="max-w-screen-lg min-h-screen px-2 py-4 m-auto">
