88be6fe5debb75f754f94259ec1b12155c03db4a
deusexmachinaa <gjfzml@gmail.com>
2023-05-25 05:34:49 +0900

    다크모드 위치변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 "use client";
-import React, { useEffect } from "react";
+import { useTheme } from "next-themes";
+import React, { useEffect, useState } from "react";
 import { create } from "zustand";
 
 export type exchangeState = {
@@ -13,6 +14,16 @@ export const useExchangeStore = create<exchangeState>((set) => ({
 }));
 
 export default function Header() {
+  const { theme, setTheme } = useTheme();
+  const [isRotating, setIsRotating] = useState(false);
+  const handleClick = () => {
+    setIsRotating(true);
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  const handleAnimationEnd = () => {
+    setIsRotating(false);
+  };
   const exchangeRate = useExchangeStore((state) => state.exchangeRate);
   const setExchangeRate = useExchangeStore((state) => state.setExchangeRate);
 
@@ -26,12 +37,25 @@ export default function Header() {
       });
   }, []);
   return (
-    <header className="p-2 border-b dark:border-neutral-700">
-      <div className="w-full max-w-screen-lg m-auto text-xs">
+    <header className="p-2 border-b dark:border-neutral-700 ">
+      <div className="w-full max-w-screen-lg m-auto text-xs flex items-center justify-between">
         {/* TODO: Fetch and display the current exchange rate */}
         <span>
           환율(USD/KRW) <b>{exchangeRate}</b>
         </span>
+        <button
+          onClick={handleClick}
+          onAnimationEnd={handleAnimationEnd}
+          className={`text-xl w-5 h-5 justify-center flex items-center bubble hover:bg-gray-200 hover:dark:bg-neutral-700 rounded-full p-2 ${
+            isRotating ? "animate-rotate" : ""
+          }`}
+        >
+          {theme === "dark" ? (
+            <i className="fa-solid fa-sun"></i>
+          ) : (
+            <i className="fa-solid fa-moon"></i>
+          )}
+        </button>
       </div>
     </header>
   );
