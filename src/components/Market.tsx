650a7e673d30ae955f7f274cdf7750b249961a79
deusexmachinaa <gjfzml@gmail.com>
2023-05-25 06:07:55 +0900

    마켓커서 default

--- src/components/Market.tsx
+++ src/components/Market.tsx
@@ -22,7 +22,7 @@ export default function Market() {
     <div className="sm:flex sm:items-center sm:justify-between">
       <div className="relative flex items-center justify-between gap-10 mb-4 text-center sm:mb-0">
         <div className="relative w-full border border-gray-200 rounded-md dark:border-neutral-700 dark:bg-neutral-800 sm:w-40">
-          <button className="w-full px-4 py-3 border border-gray-200 rounded-md dark:border-neutral-700 [&>*]:justify-center">
+          <button className="w-full px-4 py-3 border border-gray-200 rounded-md dark:border-neutral-700 [&>*]:justify-center cursor-default">
             <div className="flex items-center h-5 gap-2">
               <Image alt="upbit krw" src={upbit} width={20} height={20} />
               <span className="text-xs">업비트 KRW</span>
@@ -52,7 +52,7 @@ export default function Market() {
         </div>
         <i className="absolute text-xs -translate-x-1/2 fa-solid fa-right-left left-1/2"></i>
         <div className="relative w-full border border-gray-200 rounded-md dark:border-neutral-700 dark:bg-neutral-800 sm:w-40">
-          <button className="w-full px-4 py-3 border border-gray-200 rounded-md dark:border-neutral-700 [&>*]:justify-center">
+          <button className="w-full px-4 py-3 border border-gray-200 rounded-md dark:border-neutral-700 [&>*]:justify-center cursor-default">
             <div className="flex flex-row items-center h-5 gap-2">
               <Image alt="binance usdt" src={binance} width={20} height={20} />
               <span className="text-xs">바이낸스 USDT</span>
