9445fb56e3d48645ba2ddb4df4687da163f12857
deusexmachinaa <gjfzml@gmail.com>
2023-06-01 16:31:39 +0900

    revalidate: 0

--- src/api/getNickname.ts
+++ src/api/getNickname.ts
@@ -1,7 +1,7 @@
 "use server";
 export default async function getNickname() {
   const res = await fetch("https://nickname.hwanmoo.kr/?format=text&count=1", {
-    next: { tags: ["/"] },
+    next: { revalidate: 0 },
   });
   const nickname = await res.text();
   return nickname;
