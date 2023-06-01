16fa0a5d892990a95b8fbabb930494a11f130c98
deusexmachinaa <gjfzml@gmail.com>
2023-06-01 16:29:53 +0900

    use server

--- src/api/getNickname.ts
+++ src/api/getNickname.ts
@@ -1,5 +1,8 @@
+"use server";
 export default async function getNickname() {
-  const res = await fetch("https://nickname.hwanmoo.kr/?format=text&count=1");
+  const res = await fetch("https://nickname.hwanmoo.kr/?format=text&count=1", {
+    next: { tags: ["/"] },
+  });
   const nickname = await res.text();
   return nickname;
 }
