f9cf83cbc9e5f56d6e8e7bdb1d30848ce321d973
deusexmachinaa <gjfzml@gmail.com>
2023-06-02 20:26:48 +0900

    viewport 스케일 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,8 @@ export const metadata: Metadata = {
     url: "https://kimp.site",
     siteName: "김프사이트 - kimp.site",
   },
+  viewport:
+    "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
 };
 export default function RootLayout({
   children,
