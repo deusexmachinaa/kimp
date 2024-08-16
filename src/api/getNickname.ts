789026ae1412e57eeffa0fa7fcc7e076110e05f0
deusexmachinaa <gjfzml@gmail.com>
2024-08-17 08:29:55 +0900

    닉네임 에러처리

--- src/api/getNickname.ts
+++ src/api/getNickname.ts
@@ -1,8 +1,19 @@
 "use server";
 export default async function getNickname() {
-  const res = await fetch("https://nickname.hwanmoo.kr/?format=text&count=1", {
-    next: { revalidate: 0 },
-  });
-  const nickname = await res.text();
-  return nickname;
+  try {
+    const res = await fetch("https://nickname.hwanmoo.kr/?format=text&count=1", {
+      next: { revalidate: 0 },
+    });
+
+    if (!res.ok) {
+      // HTTP 에러 상태일 경우 기본 이름 반환
+      throw new Error("Failed to fetch nickname");
+    }
+
+    const nickname = await res.text();
+    return nickname;
+  } catch (error) {
+    // 에러 발생 시 기본 이름 반환
+    return "익명";
+  }
 }
